78de686e588430796c9c04a4705e3a242b8e62b5
Billy <bilred@users.noreply.github.com>
2024-01-18 03:01:46 +0000

    implement create activity

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
 #tracer = trace.get_tracer("home.activities")
 
@@ -12,33 +12,6 @@ class HomeActivities:
     #  span = trace.get_current_span()
     #  now = datetime.now(timezone.utc).astimezone()
     #  span.set_attribute("app.now", now.isoformat())
-
-    sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-    """)
-    print("SQL--------------")
-    print(sql)
-    print("SQL--------------")
-    with pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql)
-        # this will return a tuple
-        # the first field being the data
-        json = cur.fetchone()
-    print("-1----")
-    print(json[0])
-    return json[0]
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
     return results
